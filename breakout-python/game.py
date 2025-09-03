fe4e0f63725fa9eab664cef781aacce5605928bb
Kush-Saharan <kush.saharan29@gmail.com>
2025-09-03 19:09:09 +0530

    Made some functions

--- breakout-python/game.py
+++ breakout-python/game.py
@@ -3,7 +3,7 @@ import tkinter as tk
 class Game(tk.Frame):
 
     def __init__(self,master):
-        super.__init__(master)
+        super().__init__(master)
         #basic details of the canvas
         self.lives=3
         self.width=610
@@ -11,6 +11,24 @@ class Game(tk.Frame):
         self.canvas=tk.Canvas(self,bg='#aaaaff',width=self.width,height=self.height) #Creating the canvas
         self.canvas.pack()
         self.pack()
+
+        self.items={}   #to keep track of items which will be needed for collisions
+        self.ball=None  
+        self.paddle=Paddle(self.canvas,self.width/2,326)    #creates the paddle object
+        self.items[self.paddle.item]=self.paddle    #adds paddle to the items of the game
+
+        for x in range(5,self.width-5,75):  #creates bricks for the game
+            self.add_brick(x+37.7,50,2)
+            self.add_brick(x+37.5,70,1)
+            self.add_brick(x+37.5,90,1)
+
+        self.hud=None
+        self.setup_game()
+        self.canvas.focus_set() #makes sure your Canvas is the active widget for keyboard input
+        self.canvas.bind('<Left>',
+                         lambda _:self.paddle.move(-10))
+        self.canvas.bind('<Right>',
+                         lambda _:self.paddle.move(10))
 
     def setup_game(self):
         pass
@@ -30,11 +48,18 @@ class Game(tk.Frame):
     def update_lives(self):
         pass
 
-    def add_brick(self):
-        pass
+    def add_brick(self,x,y,hits):
+        brick=Brick(self.canvas,x,y,hits)   #adds bricks on the screen with default hits and colors
+        self.items[brick.item]=brick        #adds bricks to game items
+
 
     def add_ball(self):
-        pass
+        if self.ball is not None:   #deletes all balls that arent with paddle
+            self.ball=ball
+        paddle_coords=self.paddle.get_position()    
+        x=(paddle_coords[0]+paddle_coords[2])/2
+        ball=Ball(self.canvas,x,310)    #creates a ball object just above paddle
+        self.paddle.set_ball(self.ball) #connects the ball to the paddle until it starts moving
 
 
 class GameObject:
@@ -44,13 +69,13 @@ class GameObject:
         self.item=item
 
     def get_position(self):
-        return self.canvas.coords(self.item)
+        return self.canvas.coords(self.item)    #used to get positions of game objects
     
     def move(self,x,y):
-        self.canvas.move(self.item,x,y)
+        self.canvas.move(self.item,x,y) #used to move game objects
 
     def delete(self):
-        self.canvas.delete(self.item)
+        self.canvas.delete(self.item)   #used to delete game objects
 
 
 class Ball(GameObject):
@@ -78,33 +103,38 @@ class Paddle(GameObject):
     def __init__(self, canvas,x,y):
         self.width=80
         self.height=10
-        self.ball=None
-        item=self.canvas.create_rectangle(x-self.width/2,y-self.width/2,x+self.width/2,y+self.width/2,fill='blue')
+        self.ball=None  #the ball isnt set yet
+        item=self.canvas.create_rectangle(x-self.width/2,y-self.width/2,x+self.width/2,y+self.width/2,fill='blue')  #creates paddle
         super().__init__(canvas, item)
 
-    def set_ball(self):
-        pass
+    def set_ball(self,ball):
+        self.ball=ball
 
-    def move(self):
-        pass
+    def move(self,offset):
+        coords=self.get_position()  #First get the position of the paddle
+        width=self.canvas.winfo_width()
+        if coords[0]+offset>=0 and coords[2]+offset<=0: #checks if the paddle is between the screen
+            super().move(offset,0)
+            if self.ball is not None:   #checks if the ball is not set yet
+                self.ball.move(offset,0)
 
 
 class Brick(GameObject):
     COLORS={1:'#999999', 2:'#555555', 3:'#222222'}
 
     def __init__(self, canvas, x,y,hits):
-        #creates basic bricks depending on number of hits the brick has
+        #creates basic bricks
         self.width=80
         self.height=20
-        color=Brick.COLORS[hits]
+        color=Brick.COLORS[hits]    #defines different colors for different hits
         item=self.canvas.create_rectangle(x-self.width/2,y-self.width/2,x+self.width/2,y+self.width/2,fill=color,tags='brick')
         super().__init__(canvas, item)
 
     def hit(self):
         pass
 
-
-root=tk.Tk()
-root.title('Breakout')
-game=Game(root)
-game.mainloop()
+if __name__=='__main__':
+    root=tk.Tk()
+    root.title('Breakout')
+    game=Game(root)
+    game.mainloop()
